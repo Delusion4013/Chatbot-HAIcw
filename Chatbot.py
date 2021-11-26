dee290c38437c9559a3c517bd9e699d20d38047c
Delusion4013 <594431233@qq.com>
2021-11-26 16:16:50 +0800

    Extracted identity management as a sub system.

--- Chatbot.py
+++ Chatbot.py
@@ -8,6 +8,7 @@ from nltk.tree import Tree;
 import systems.game as game
 import systems.information_retrieval as IR
 import systems.small_talk as ST
+import systems.identity_managment as IM
 
 
 class Chatbot():
@@ -21,7 +22,7 @@ class Chatbot():
     name_get = False
     
     # Bot profile
-    robot_name = "ROBO"
+    robot_name = "Jarvis"
 
     # Constants for text matching
     NAME_INPUTS = ()
@@ -31,43 +32,69 @@ class Chatbot():
     GOODBYE_RESPONSES = ()
 
     # Data path
-    user_profile_path = "data/user_profile.txt"
+    user_profile_path = "data/user_profile.json"
 
     def __init__(self, robot_name):
         # Keyword Matching
         self.NAME_INPUTS = ("my name is", "i am", "call me", "i'm")
         self.IDENTITY_FIRST_RESPONSES = ("I will remember you ", "I will keep it in my database ", "Got you! ")
         self.IDENTITY_SECOND_RESPONSES = ("I remember you, you are ", "I find it in my database, ", "Nice seeing you again! ")
-        self.IDENTITY_INPUTS = ("what is my name?", "who am i")
+        self.IDENTITY_INPUTS = ("what is my name?", "who am i?","what is my name", "who am i")
         self.GOODBYE_RESPONSES = ("Bye! Take care.", "See you soon!", "Have a nice day!", "Enjoy your day!")
         self.robot_name = robot_name
+
+    def show_menu(self):
+        """
+            Prompt the user with help information on the chatbot's capability.
+        """
+        print("1) if you want to ask questions, type [QA]")
+        print("2) if you want to play games, type [games]")
+        print("3) if you want to order something, type [transaction]")
+
+    def robo_utter(self, utterance):
+        """
+            Customize chatbot's prompts by using pre-set robot name.
+        """
+        print(self.robot_name + ": " + utterance)
+
+    def update_database(self):
+        # if (self.)
+        # open(self.user_profile_path, 'rw')
+        return
 
     def goodbye(self):
         return random.choice(self.GOODBYE_RESPONSES)
 
-    def identity(self, sentence):
-        words = word_tokenize(sentence)
-        # print(words)
-        tokens_without_sw = [word.title() for word in words
-                                if word not in stopwords.words()] # stop words removal
-        # print(tokens_without_sw)
-        postags = pos_tag(tokens_without_sw)
-        word_tags = nltk.ne_chunk(postags, binary=False)
-        for tag in word_tags:
-            if type(tag) == Tree and tag.label() == 'PERSON':
-                for item in tag:
-                    self.user_name.append(item[0] + '')
-        print(self.user_name)
+    def intent_match(self, user_response):
+        """
+            Matches user's intent based on keywords and patterns. 
+            Then invoke the relevant sub systems.
+        """
 
-        if self.user_name:
+        if (user_response == 'menu' or user_response == 'help'):
+            self.show_menu()
+        elif (user_response == 'qa' or user_response == 'IR'):
+            IR.pipeline(self.robot_name)
+        elif (user_response == 'games'):
+            self.user_name = game.pipeline(self.user_name, self.robot_name)
             self.name_get = True
-        if self.name_get:
-            print(type(self.user_name))
-            self.user_name = ' '.join(self.user_name)
-            print("ROBO: " + random.choice(self.IDENTITY_FIRST_RESPONSES) + self.user_name)
+        elif (ST.pipeline(user_response) != None):
+            self.robo_utter(ST.pipeline(user_response))
+        elif (user_response =='thanks' or user_response == 'thank you'):
+            self.robo_utter("You are welcome.")
+        elif(user_response in self.IDENTITY_INPUTS):
+            (self.user_name, self.name_get) = IM.pipeline(self.robot_name, self.user_name, self.name_get)
+        # elif (user_response == 'transaction'):
+        #     # transaction system
+        #     print('transaction system')
+        else:
+            self.robo_utter("I am sorry! I don't understand you.")
+        return
 
     def general_pipeline(self):
-
+        """
+            The general procedure for chatbot taking in user input and generates response.
+        """
         # Propose welcome information
         print(self.robot_name + ": Hi! This is " + self.robot_name + ", how can I help you?")
 
@@ -83,53 +110,5 @@ class Chatbot():
                 flag = False
                 self.robo_utter(self.goodbye())
                 self.update_database()
-        return
-
-    def show_menu(self):
-        print("1) if you want to ask questions, type [QA]")
-        print("2) if you want to play games, type [games]")
-        print("3) if you want to do some small talks, type [small talk]")
-        print("4) if you want to order something, type [transaction]")
-
-    def intent_match(self, user_response):
-        """
-            Matches user's intent based on keywords and patterns. 
-            Then invoke the relevant sub systems.
-        """
-
-        if (user_response == 'menu' or user_response == 'help'):
-            self.show_menu()
-        elif (user_response == 'qa' or user_response == 'IR'):
-            IR.pipeline()
-        elif (user_response == 'games'):
-            self.user_name = game.pipeline(self.user_name, self.robot_name)
-            self.name_get = True
-        # elif (user_response == 'transaction'):
-        #     # transaction system
-        #     print('transaction system')
-        elif (ST.pipeline(user_response) != None):
-            self.robo_utter(ST.pipeline(user_response))
-        elif (user_response =='thanks' or user_response == 'thank you'):
-            print("ROBO: You are welcome.")
-        elif(user_response in self.IDENTITY_INPUTS):
-            if self.name_get == True:
-                self.robo_utter(random.choice(self.IDENTITY_SECOND_RESPONSES) + self.user_name)
-            elif self.name_get == False:
-                self.robo_utter("You didn't tell me your name! Please tell me and I will keep it down.")
-                user_response = input("YOU: ")
-                self.identity(user_response)
-        else:
-            self.robo_utter("I am sorry! I don't understand you.")
-        return
-
-    def robo_utter(self, utterance):
-        """
-            Customize chatbot's prompts by using pre-set robot name.
-        """
-        print(self.robot_name + ": " + utterance)
-
-    def update_database(self):
-        # if (self.)
-        # open(self.user_profile_path, 'rw')
         return
 
---
+++ systems/identity_managment.py
@@ -0,0 +1,73 @@
+from os import name
+import random
+import nltk
+from nltk.corpus import stopwords
+from nltk.tag import pos_tag
+from nltk.tokenize import word_tokenize
+from nltk.tree import Tree;
+
+
+def identity(sentence):
+    """
+        Inspect a sentence to see if a name entity contains.
+
+        Returns: a tuple with 
+                    - the acquired user name
+                    - a boolean variable indicating whether a name was found
+    """
+    user_name = []
+    name_get = False
+
+    words = word_tokenize(sentence)
+    tokens_without_sw = [word.title() for word in words
+                            if word not in stopwords.words()] # stop words removal
+    postags = pos_tag(tokens_without_sw)
+    
+    # Use chunking mechanism to find names
+    word_tags = nltk.ne_chunk(postags, binary=False)
+    for tag in word_tags:
+        if type(tag) == Tree and tag.label() == 'PERSON':
+            for item in tag:
+                user_name.append(item[0] + '')
+
+    if user_name:
+        name_get = True
+    if name_get:
+        user_name = ' '.join(user_name)
+        return (user_name, name_get)
+    
+    return ("", name_get)
+
+def pipeline(robot_name, userName, nameGet):
+    """
+        General procedure for this sub system.
+    """
+
+    IDENTITY_FIRST_RESPONSES = ("I will remember you ", "I will keep it in my database ", "Got you! ")
+    IDENTITY_SECOND_RESPONSES = ("I remember you, you are ", "I find it in my database, ", "Nice seeing you again! ")
+    name_get = nameGet
+    user_name = userName
+
+    # If the name was stored beforehand
+    if name_get == True:
+        print(robot_name + ": " + random.choice(IDENTITY_SECOND_RESPONSES) + user_name)
+    else:
+        # Obtain the name using identity() method.
+        print(robot_name + ": You didn't tell me your name! Please tell me and I will keep it down.")
+        # Using the while loop to enable users to re-enter.
+        while name_get == False:
+            user_response = input("> ")
+            if user_response == "quit" or user_response == "q":
+                break
+            (user_name, name_get) = identity(user_response)
+            if name_get == True:
+                print(robot_name + ": " + random.choice(IDENTITY_FIRST_RESPONSES) + user_name)
+                break
+            else:
+                print(robot_name + ": " + "No name detected in former input. Please try again...")
+                print(robot_name + ": " + "Type [quit] to terminate this process.")
+    return (user_name, name_get)
+
+# To test the sub system, invoke pipeline here
+# pipeline("Jarvis", "Chenkai Hu", True)
+# pipeline("Jarvis", "", False)
